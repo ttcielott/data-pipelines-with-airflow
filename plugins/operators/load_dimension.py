491ae377db6ea57e967d49f859bf3e2ad7683b88
Haneul Kim <ttcielott@gmail.com>
2024-02-26 23:39:17 +0000

    added 'truncate_existing_records' parameter

--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -5,23 +5,36 @@ from airflow.utils.decorators import apply_defaults
 class LoadDimensionOperator(BaseOperator):
 
     ui_color = '#80BD9E'
-    sql_template = 'INSERT INTO {} {};'
+    truncate_sql_template = 'TRUNCATE TABLE {};'
+    append_sql_template = 'INSERT INTO {} {};'
+
 
     @apply_defaults
     def __init__(self,
                  redshift_conn_id = 'redshift',
                  table_name = '',
                  content_sql_template = '',
+                 truncate_existing_records = True,
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
         self.redshift_conn_id = redshift_conn_id
         self.table_name = table_name
         self.content_sql_template = content_sql_template
+        self.truncate_existing_records = truncate_existing_records
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
         redshift = PostgresHook(postgres_conn_id = self.redshift_conn_id)
-        complete_sql_template = LoadDimensionOperator.sql_template.format(self.table_name, self.content_sql_template)
+        self.log.info(f'Load Dimension for Table Name, {self.table_name} : PostgresHook Object, redshift instantiated')
+        
+        if self.truncate_existing_records:
+            trucate_sql_filled = LoadDimensionOperator.truncate_sql_template.format(self.table_name)
+            self.log.info(f'Load Dimension for Table Name, {self.table_name} : running Truncate SQL statement - started')
+            redshift.run(trucate_sql_filled)
+            self.log.info(f'Load Dimension for Table Name, {self.table_name} : running Truncate SQL statement - completed')
+
+        complete_sql_template = LoadDimensionOperator.append_sql_template.format(self.table_name, self.content_sql_template)
+        self.log.info(f'Load Dimension for Table Name, {self.table_name} : running Insert SQL statement - started')
         redshift.run(complete_sql_template)
+        self.log.info(f'Load Dimension for Table Name, {self.table_name} : running Insert SQL statement - completed')
 
