c1dab6f3c6c5bbf1b4a8eca009a602ebaa64d2f3
Haneul Kim <ttcielott@gmail.com>
2024-02-26 22:52:29 +0000

    moved 'catchup' to default_args

--- dags/final_project.py
+++ dags/final_project.py
@@ -13,17 +13,22 @@ default_args = {
     'depends_on_past': False,
     'retries': 3,
     'retry_delay': timedelta(minutes=5),
-    'email_on_failure': False
+    'email_on_failure': False,
+    'catchup' : False
+
     
 }
 
 @dag(
     default_args=default_args,
     description='Load and transform data in Redshift with Airflow',
-    schedule_interval='0 * * * *',
-    catchup = False
+    schedule_interval='0 * * * *'
 )
 def final_project():
+    """
+    The function `final_project` defines a series of tasks for an ETL pipeline using Apache Airflow
+    operators to load data from S3 to Redshift and perform data quality checks.
+    """
 
     start_operator = DummyOperator(task_id='Begin_execution')
 
@@ -42,53 +47,48 @@ def final_project():
 
     load_songplays_table = LoadFactOperator(
         task_id='Load_songplays_fact_table',
-	table_name = 'songplays',
+        table_name = 'songplays',
         sql_template = SqlQueries.songplay_table_insert
     )
 
     load_user_dimension_table = LoadDimensionOperator(
         task_id='Load_user_dim_table',
         table_name = 'users',
+        truncate_existing_records = True,
         content_sql_template = SqlQueries.user_table_insert
     )
 
     load_song_dimension_table = LoadDimensionOperator(
         task_id='Load_song_dim_table',
         table_name = 'songs',
+        truncate_existing_records = True,
         content_sql_template = SqlQueries.song_table_insert
     )
 
     load_artist_dimension_table = LoadDimensionOperator(
         task_id='Load_artist_dim_table',
         table_name = 'artists',
+        truncate_existing_records = True,
         content_sql_template = SqlQueries.artist_table_insert
     )
 
     load_time_dimension_table = LoadDimensionOperator(
         task_id='Load_time_dim_table',
         table_name = 'time',
+        truncate_existing_records = True,
         content_sql_template = SqlQueries.time_table_insert
     )
 
     run_quality_checks = DataQualityOperator(
-        task_id='Run_data_quality_checks',
-        table_name='songplays'
+        task_id='Run_data_quality_checks'
     )
 
     end_operator = DummyOperator(task_id='End_execution')
 
-    start_operator >> stage_songs_to_redshift
-    start_operator >> stage_events_to_redshift
-    stage_songs_to_redshift >> load_songplays_table
-    stage_events_to_redshift >> load_songplays_table
-    load_songplays_table >> load_user_dimension_table
-    load_songplays_table >> load_song_dimension_table
-    load_songplays_table >> load_artist_dimension_table
-    load_songplays_table >> load_time_dimension_table
-    load_user_dimension_table >> run_quality_checks
-    load_song_dimension_table >> run_quality_checks
-    load_artist_dimension_table >> run_quality_checks
-    load_time_dimension_table >> run_quality_checks
+    start_operator >> [stage_songs_to_redshift, stage_events_to_redshift]
+    [stage_songs_to_redshift, stage_events_to_redshift] >> load_songplays_table
+    load_songplays_table >> [load_user_dimension_table, load_song_dimension_table, load_artist_dimension_table, load_time_dimension_table]
+    [load_user_dimension_table, load_song_dimension_table, load_artist_dimension_table, load_time_dimension_table] >> run_quality_checks
     run_quality_checks >> end_operator
 
 final_project_dag = final_project()
