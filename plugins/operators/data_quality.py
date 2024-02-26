ccb66e6aed5b0f73bc26743ec51d0b5b8ac67ace
Haneul Kim <ttcielott@gmail.com>
2024-02-26 23:37:56 +0000

    parameterised query and expected result in DataQualityOperator

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -2,6 +2,8 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
+# This Python class `DataQualityOperator` performs data quality checks on tables in a Redshift
+# database to ensure correct table names and minimum row counts.
 class DataQualityOperator(BaseOperator):
 
     ui_color = '#89DA59'
@@ -9,24 +11,33 @@ class DataQualityOperator(BaseOperator):
     @apply_defaults
     def __init__(self,
                  redshift_conn_id = "redshift",
-                 table_name = "",
+                 dq_check = "",
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
         self.redshift_conn_id = redshift_conn_id
-        self.table_name = table_name
+        self.dq_check = dq_check
 
     def execute(self, context):
-        self.log.info('DataQualityOperator not implemented yet')
         redshift = PostgresHook(postgres_conn_id = self.redshift_conn_id)
-        records = redshift.get_records(f"SELECT COUNT(*) FROM {self.table_name}")
+        self.log.info(f'Data Quality Check : PostgresHook Object, redshift instantiated')
 
-        if len(records) < 1 or len(records[0]) < 1:
-            raise ValueError(f'Data Quality on table {self.table_name} check failed. {self.table_name} returns no result.')
-        
-        num_records = records[0][0]
+        for pair in self.dq_check:
+            test_name = list(pair.keys())[0]
+            test_sql = list(pair.values())[0]
+            expected_result = list(pair.values())[1]
+            self.log.info(f'Data Quality Check : {test_name} - started')
 
-        if num_records < 1:
-            raise ValueError(f'Data quality check failed. {self.table_name} contained 0 rows')
+            records = redshift.get_records(test_sql)
+
+            if len(records) < 1 or len(records[0]) < 1:
+                raise ValueError(f'Data Quality Check: {test_name} failed. {test_name} returns no result.')
+                
+            record_value = records[0][0]
+
+            if record_value != expected_result:
+                raise ValueError(f'Data quality Check: {test_name} failed. Returned Value: {record_value} / Expected Result: {expected_result}')
         
-        self.log.info(f"Data quality on table {self.table_name} check passed with {records[0][0]} records")
+            self.log.info(f'Data Quality Check : {test_name} - completed')
+
+            
